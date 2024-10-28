eebb6a5f9d7ed7ecec3b8deede6755c9c3befe19
Nishant. <98388538+NishantKB@users.noreply.github.com>
2024-10-29 00:47:13 +0545

    added few codes

---
+++ average_of_two_floats.py
@@ -0,0 +1,4 @@
+num1 = float(input("Enter the first floating point number: "))
+num2 = float(input("Enter the second floating point number: "))
+average = (num1 + num2) / 2
+print("The average is:", average)
---
+++ square_area_calculator.py
@@ -0,0 +1,3 @@
+side = float(input("Enter the side length of the square: "))
+area = side * side
+print("The area of the square is:", area)
